ca751bdfa87f29f290dcccc65459f09903bb9752
Tsubz <marcantoine.bazin@gmail.com>
2017-05-30 13:34:55 +0200

    Fork try
    Truing to fork correctly

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ function randomPick(){
     return listChoices[parseInt(Math.random()*3)];
 }
 
+var hist = [];
+
 function confrontation(server, player){
     if(server==="rock"){
         if(player === "rock"){
@@ -26,7 +28,6 @@ function confrontation(server, player){
     }else if(server==="paper"){
         if(player === "rock"){
             return "You are a big looser, I picked paper.";
-            
         }else if(player === "paper"){
             return "It's a draw, we both picker paper.";
         }else if(player==="scissors"){
@@ -47,8 +48,65 @@ function confrontation(server, player){
     }
 }
 
+
+function recordHist(server, player){
+    if(server==="rock"){
+        if(player === "rock"){
+            hist.push(["rock", "rock", "Draw"]);
+        }else if(player === "paper"){
+            hist.push(["rock", "paper", "Player"]);
+        }else if(player==="scissors"){
+            hist.push(["rock", "scissors", "Server"]);
+        } else {
+            return "You are a cheater, " + player + " is not allowed.";
+        }
+    }else if(server==="paper"){
+        if(player === "rock"){
+            hist.push(["paper", "rock", "Server"]);            
+        }else if(player === "paper"){
+            hist.push(["paper", "paper", "Draw"]);
+        }else if(player==="scissors"){
+            hist.push(["paper", "scissors", "Player"]);
+        } else {
+            return "You are a cheater, " + player + " is not allowed.";
+        }
+    }else {
+        if(player === "rock"){
+            hist.push(["scissors", "rock", "Player"]);     
+        }else if(player === "paper"){
+            hist.push(["scissors", "paper", "Server"]);
+        }else if(player==="scissors"){
+            hist.push(["scissors", "scissors", "Draw"]);
+        } else {
+            return "You are a cheater, " + player + " is not allowed.";
+        }
+    }
+    /*
+    var tableHtml = "<table border='1'>";
+ 
+for (var i = 0; i < hist.length; i++) {
+   tableHtml += "<tr><td>"+hist[i][1]+"</td></tr>";
+}
+    
+    tableHtml += "</table>";
+*/
+    return hist;
+}
+
 app.get('/', function (req, res) {
-    res.render('home',{userName:"Christophe"});
+    res.render('home',{userName:"Marc-Antoine"});
+});
+
+app.get('/game', function (req, res) {
+    res.render('game',{userName:"Marc-Antoine", gameHistory:hist});
+});
+
+app.get('/help', function (req, res) {
+    res.render('help',{userName:"Marc-Antoine"});
+});
+
+app.get('/history', function (req, res) {
+    res.render('history',{userName:"Marc-Antoine", gameHistory:hist});
 });
 
 app.get('/game/:playerchoice/', function (req, res) {
@@ -58,7 +116,8 @@ app.get('/game/:playerchoice/', function (req, res) {
     res.render("game",{
         serverChoice: serverPick,
         playerChoice : playerPick,
-        gameResult:confrontation(serverPick,playerPick)
+        gameResult:confrontation(serverPick,playerPick),
+        gameHistory:recordHist(serverPick,playerPick),
     });
     
 })
